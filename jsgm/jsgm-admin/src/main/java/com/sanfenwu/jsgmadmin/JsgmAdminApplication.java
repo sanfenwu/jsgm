a9b3af7ea7386ad306f98d79d548b499eeff080a
sanfenwu <18369655060@163.com>
2022-04-14 10:59:30 +0800

    项目提交

---
+++ jsgm/jsgm-admin/src/main/java/com/sanfenwu/jsgmadmin/JsgmAdminApplication.java
@@ -0,0 +1,13 @@
+package com.sanfenwu.jsgmadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsgmAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsgmAdminApplication.class, args);
+    }
+
+}
---
+++ jsgm/jsgm-admin/src/test/java/com/sanfenwu/jsgmadmin/JsgmAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanfenwu.jsgmadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsgmAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
